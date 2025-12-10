b9bd2e31f390d4e1bf228f70130567dc27bdec3f
Timo <timo.vanderven04@gmail.com>
2025-12-10 12:16:36 +0100

    feat: add folds query

---
+++ queries/folds.scm
@@ -0,0 +1,18 @@
+[
+  (workspace_declaration)
+  (model_declaration)
+  (group_declaration)
+  (person_declaration)
+  (software_system_declaration)
+  (container_declaration)
+  (component_declaration)
+  (configuration_declaration)
+  (views_declaration)
+  (system_context_view_declaration)
+  (container_view_declaration)
+  (dynamic_view_declaration)
+  (styles_declaration)
+  (element_declaration)
+  (relationship_declaration)
+] @fold
+
